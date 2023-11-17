bc1ed9f46255efee555a3aa09ac3f4ba97891abd
Jakub Jelinek <vilouskubajj@gmail.com>
2023-11-17 15:20:54 +0000

    sol.vertices start at 0, but vertices start at 1

--- apps/bp/dmdgp/pruningtest.cpp
+++ apps/bp/dmdgp/pruningtest.cpp
@@ -7,13 +7,15 @@ bool pruningTest(int vertexId, const DMDGPMaps &maps, DMDGPSol &sol, DMDGPVertex
     double eps, dist, diff;
     // Epsilon for precision
     eps = 0.001;
-    for (int i = 0; i < j && !prune; i++)
+    // Note(kubajj): Vertices are numbered 1..n,
+    // but sol.vertices starts at 0
+    for (int i = 1; i < j && !prune; i++)
     {
         auto idistance = maps.distanceMap.find({i, vertexId});
         // There is a distance between i and current vertex
         if (idistance != maps.distanceMap.end())
         {
-            dist = calculateDistance(sol.vertices[i], currentPosition);
+            dist = calculateDistance(sol.vertices[i - 1], currentPosition);
             diff = dist - idistance->second;
             if (diff < 0.0)
                 diff = -diff;
