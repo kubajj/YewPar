7fb63013d2794a4a97303bb59e9715b79349b496
Jakub Jelinek <vilouskubajj@gmail.com>
2024-01-31 13:56:26 +0000

    Test of C io in main

--- apps/bp/mdjeep/main.cpp
+++ apps/bp/mdjeep/main.cpp
@@ -20,48 +20,66 @@ double INFTY = 1.e+30;
 
 int hpx_main(hpx::program_options::variables_map &opts)
 {
-    int i, n, n0, m, mexact;
-    int fidx, verr;
-    int it, flag;
-    size_t nlines, linelen, wordlen;
-    bool clique, smallsine;
-    bool check_consec;
-    double **X;
-    double obj, cosine;
-    char *errmsg;
-    char *line;
-    char *timestring;
-    VERTEX *v;
-    SEARCH S;
-    OPTION op;
-    INFORMATION info;
-    unsigned long typelist;
-    struct timeval t1, t2;
-    FILE *input;
+  int i, n, n0, m, mexact;
+  int fidx, verr;
+  int it, flag;
+  size_t nlines, linelen, wordlen;
+  bool clique, smallsine;
+  bool check_consec;
+  double **X;
+  double obj, cosine;
+  char *errmsg;
+  char *line;
+  char *timestring;
+  VERTEX *v;
+  SEARCH S;
+  OPTION op;
+  INFORMATION info;
+  unsigned long typelist;
+  struct timeval t1, t2;
+  FILE *input;
 
-    // hpx::program_options::notify(opts);
+  std::cout << "YewPar DDGP solver based on MDjeep" << std::endl;
 
-    std::cout << "YewPar DDGP solver based on MDjeep" << std::endl;
-
-    auto start_time = std::chrono::steady_clock::now();
-
-    /*
-      Main body
-    */
-
-    auto overall_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time);
-
-    hpx::cout << "cpu = " << overall_time.count() << std::endl;
-
+  auto inputFile = opts["mdfile"].as<std::string>();
+  input = fopen(inputFile, "r");
+  if (input == NULL)
+  {
+    fprintf(stderr, "mdjeep: error while opening MDfile '%s';", inputFile);
     return hpx::finalize();
+  };
+
+  // reading the MDfile and setting up the main options and infos
+  errmsg = readMDfile(input, &op, &info);
+
+  // any error occurred while reading MDfile?
+  if (errmsg != NULL)
+  {
+    fprintf(stderr, "%s\n", errmsg);
+    free(errmsg);
+    return hpx::finalize();
+  };
+  fclose(input);
+  input = NULL;
+  auto start_time = std::chrono::steady_clock::now();
+
+  /*
+    Main body
+  */
+
+  auto overall_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time);
+
+  hpx::cout << "cpu = " << overall_time.count() << std::endl;
+
+  return hpx::finalize();
 }
 
 int main(int argc, char *argv[])
 {
-    hpx::program_options::options_description
-        desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
+  hpx::program_options::options_description
+      desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
 
-    // clang-format off
+  // clang-format off
     desc_commandline.add_options()
       ( "skeleton",
         hpx::program_options::value<std::string>()->default_value("seq"),
@@ -71,11 +89,11 @@ int main(int argc, char *argv[])
         hpx::program_options::value<std::string>()->required(),
         "Where to find the MDFile which contains specification of the instance and necessary fields"
       );
-    // clang-format on
+  // clang-format on
 
-    YewPar::registerPerformanceCounters();
+  YewPar::registerPerformanceCounters();
 
-    hpx::init_params args;
-    args.desc_cmdline = desc_commandline;
-    return hpx::init(argc, argv, args);
+  hpx::init_params args;
+  args.desc_cmdline = desc_commandline;
+  return hpx::init(argc, argv, args);
 }
