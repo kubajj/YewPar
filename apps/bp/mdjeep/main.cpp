b8b11a2d7886739b77226706d86e6c7c8cacc041
Jakub Jelinek <vilouskubajj@gmail.com>
2024-01-31 14:19:08 +0000

    Add main.c up to line 503 (right before calling bp)

--- apps/bp/mdjeep/main.cpp
+++ apps/bp/mdjeep/main.cpp
@@ -206,6 +206,225 @@ int hpx_main(hpx::program_options::variables_map &opts)
 
   // printing instance details
   fprintf(stderr, "mdjeep: instance file '%s' read: %d vertices / %d distances\n", info.filename, n, m);
+
+  // verifying whether all distances are exact (and precise)
+  if (m == mexact)
+  {
+    fprintf(stderr, "mdjeep: the instance contains only 'exact' distances\n");
+    if (info.method == 0)
+    {
+      // we'll invoke bp_exact if at least 90% of the distances are "very precise"
+      if (totalNumberOfPreciseDistances(n, v, 14) > 0.90 * mexact)
+      {
+        op.r = 0.0;
+        info.exact = true;
+        fprintf(stderr, "mdjeep: the resolution parameter and the refinement method have been disabled\n");
+      };
+    };
+  };
+
+  // error if no refinement method was specified for bp, and the instance does not contain enough precise distances
+  if (!info.exact)
+  {
+    if (info.method == 0)
+    {
+      if (info.refinement == -1)
+      {
+        fprintf(stderr, "mdjeep: error: no refinement method specified for bp (instance contains interval distances)\n");
+        free(v);
+        return hpx::finalize();
+      };
+    };
+  };
+
+  // checking whether the first three instance vertices form a clique (prerequisite for bp)
+  if (info.method == 0)
+  {
+    clique = initialClique(n, v, op.eps);
+    if (!clique)
+    {
+      fprintf(stderr, "mdjeep: error: the first three vertices of the input instance do not form a clique\n");
+      fprintf(stderr, "               the instance cannot be discretized\n");
+      free(v);
+      return hpx::finalize();
+    };
+  };
+
+  // checking whether the input instance is discretizable (prerequisite for bp)
+  if (info.method == 0)
+  {
+    i = isDDGP(n, v, op.eps, clique);
+    if (i != 0)
+    {
+      fprintf(stderr, "mdjeep: error: the input instance is not discretizable\n");
+      fprintf(stderr, "               not enough references for vertex %d (should have at least 3, at least 2 exact)\n", n0 + i);
+      fprintf(stderr, "               stopping here... other necessary distances may be unavailable\n");
+      free(v);
+      return hpx::finalize();
+    };
+  };
+
+  // if bp is selected, we know that the input instance is discretizable
+  if (info.method == 0)
+    fprintf(stderr, "mdjeep: the input instance is discretizable\n");
+
+  // checking the consecutivity assumption (optional)
+  if (info.method == 0)
+  {
+    if (info.exact || check_consec)
+    {
+      info.consec = isDMDGP(n, v, op.eps, true);
+      fprintf(stderr, "mdjeep: the instance ");
+      if (info.consec)
+        fprintf(stderr, "satisfies ");
+      else
+        fprintf(stderr, "does not satisfy ");
+      fprintf(stderr, "the consecutivity assumption\n");
+    };
+  };
+
+  // preparing for calling bp method
+  if (info.method == 0)
+  {
+    // memory allocation for S.refs
+    S.refs = (triplet *)calloc(n, sizeof(triplet));
+
+    // initializing all reference triplets to null
+    for (i = 0; i < n; i++)
+      S.refs[i] = nullTriplet();
+
+    // the definition of the reference vertices depends on the presence of interval distances
+    // for instances with exact distances only: the code below only verifies that the flattest triplet is not "too flat"
+    smallsine = false;
+    for (i = 3; i < n; i++)
+    {
+      if (info.exact || onlyPreciseDistances(v[i].ref, 14))
+      {
+        // triplet of references with exact distances
+        cosine = 0.0;
+        S.refs[i] = findReferencesExactCase(i, v, op.eps, &cosine);
+        if (isNullTriplet(S.refs[i]))
+        {
+          fprintf(stderr, "mdjeep: internal error: it was verified that the discretization assumptions were satisfied but they are actually not\n");
+          free(S.refs);
+          free(v);
+          return hpx::finalize();
+        };
+        if (cosine == 0.0)
+        {
+          fprintf(stderr, "mdjeep: error: one triplet of reference vertices forms a flat angle; no alternative triplet available\n");
+          free(S.refs);
+          free(v);
+          return hpx::finalize();
+        };
+        if (fabs(sqrt(1.0 - cosine * cosine)) < op.eps)
+          smallsine = true;
+      }
+      else
+      {
+        // triplet of references with one interval distance
+        S.refs[i] = findReferencesIntervalCase(i, v, op.eps);
+        if (isNullTriplet(S.refs[i]))
+        {
+          fprintf(stderr, "mdjeep: internal error: it was verified that the discretization assumptions were satisfied but they are actually not\n");
+          free(S.refs);
+          free(v);
+          return hpx::finalize();
+        };
+      };
+    };
+    if (smallsine)
+    {
+      fprintf(stderr, "mdjeep: WARNING: some triplets of reference vertices form a angle whose sine is very close to zero (tolerance is %g)\n", op.eps);
+    };
+  };
+
+  // memory allocation for the solution X
+  X = allocateMatrix(3, n);
+
+  // preparing for calling spg method
+  if (info.method == 1)
+  {
+    // reading the starting point from a text file (with predefined format)
+    input = fopen(info.start, "r");
+    if (input == NULL)
+    {
+      fprintf(stderr, "mdjeep: error while opening file containing starting point for spg\n");
+      free(X);
+      free(v);
+      return hpx::finalize();
+    };
+    if (readStartingPoint(input, n, X) != n)
+    {
+      fprintf(stderr, "mdjeep: error while reading starting point for spg, it seems it doesnt contain the expected number of vertex positions\n");
+      free(X);
+      free(v);
+      return hpx::finalize();
+    };
+  };
+
+  // looking for symmetries (even when main method is spg)
+  S.sym = (bool *)calloc(n, sizeof(bool));
+  fprintf(stderr, "mdjeep: checking symmetries ... ");
+  findSymmetries(n, v, S.sym);
+  fprintf(stderr, "layers:");
+  for (i = 0; i < n; i++)
+    if (S.sym[i])
+      fprintf(stderr, " %d", n0 + i);
+  fprintf(stderr, "\n");
+
+  // counting the maximum number of digits for monitor (optional)
+  if (op.monitor)
+  {
+    if (info.method == 0)
+      info.ndigits = numberOfDigits(n);
+    else
+      info.ndigits = numberOfDigits(op.maxit);
+  };
+
+  // setting up output filename (if necessary)
+  if (op.print != 0)
+    info.output = removExtension(info.filename);
+
+  // message about additional memory allocation
+  fprintf(stderr, "mdjeep: allocating memory ...");
+
+  // memory allocation for the arrays in SEARCH (for both bp and spg)
+  S.pX = allocateMatrix(3, n);
+  S.lX = allocateMatrix(3, n);
+  S.uX = allocateMatrix(3, n);
+  S.y = allocateVector(m);
+  S.gy = allocateVector(m);
+  S.sy = allocateVector(m);
+  S.yp = allocateVector(m);
+  S.gyp = allocateVector(m);
+  S.gX = allocateMatrix(3, n);
+  S.sX = allocateMatrix(3, n);
+  S.Xp = allocateMatrix(3, n);
+  S.gXp = allocateMatrix(3, n);
+  S.DX = allocateMatrix(3, n);
+  S.YX = allocateMatrix(3, n);
+  S.ZX = allocateMatrix(3, n);
+  S.Dy = allocateVector(m);
+  S.Yy = allocateVector(m);
+  S.Zy = allocateVector(m);
+  S.memory = allocateVector(n);
+  fprintf(stderr, " done\n");
+
+  // setting up value for pi
+  S.pi = 3.14159265358979323846;
+
+  // preparing for calling spg method (continues)
+  if (info.method == 1)
+  {
+    // defining boxes necessary for spg
+    for (i = 0; i < n; i++)
+    {
+      createBox(i, X, op.be, S.lX, S.uX);
+      expandBounds(i, v, S.lX, S.uX, op.be, op.eps);
+    };
+  };
+
   /*
     Main body
   */
